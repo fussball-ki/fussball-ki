9bd6fae7bb983108090cac8f99074b955cf349cd
fussball-ki <johannes.pichler98@gmx.at>
2026-02-22 13:32:37 +0100

    Add files via upload

---
+++ tipps.js
@@ -0,0 +1,7 @@
+const tipps = [
+    { "spiel": "Bayern vs Dortmund", "wahrscheinlichkeit": 71.23, "risiko": "green" },
+    { "spiel": "Leverkusen vs Frankfurt", "wahrscheinlichkeit": 54.12, "risiko": "yellow" },
+    { "spiel": "Köln vs Mainz", "wahrscheinlichkeit": 42.00, "risiko": "red" }
+];
+
+console.log('tipps.js wurde geladen!');
